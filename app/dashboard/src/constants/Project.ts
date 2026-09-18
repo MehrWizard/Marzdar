1d9eaf814d7b5a2b8093213fe124fb89b26e2a24
MehrWizard <328762575+MehrWizard@users.noreply.github.com>
2026-09-18 16:55:53 +0330

    feat(ui): update branding and repository links to MehrWizard/Marzdar

--- app/dashboard/src/constants/Project.ts
+++ app/dashboard/src/constants/Project.ts
@@ -1,3 +1,3 @@
-export const REPO_URL = "https://github.com/Gozargah/Marzban";
-export const ORGANIZATION_URL = "https://github.com/Gozargah";
-export const DONATION_URL = "https://github.com/Gozargah/Marzban#donation";
+export const REPO_URL = "https://github.com/MehrWizard/Marzdar";
+export const ORGANIZATION_URL = "https://github.com/MehrWizard";
+export const DONATION_URL = "https://github.com/MehrWizard/Marzdar#donation";
